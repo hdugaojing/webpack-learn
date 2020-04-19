c064cf8c9e92ae7307a1a704d3554de74e815658
Colin <colingaojing@163.com>
2020-04-19 21:52:20 +0800

    feat: 引入包组成可视化工具

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
     devtool: 'inline-source-map',
@@ -8,5 +9,8 @@ module.exports = merge(common, {
         contentBase: './dist',
         // 开启HMR
         hot: true
-    }
+    },
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ]
 })
